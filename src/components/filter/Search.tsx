6b1b2a911f50058248d099e56f08659e788854ab
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-13 22:59:08 -0300

    search text works

--- src/components/filter/Search.tsx
+++ src/components/filter/Search.tsx
@@ -3,24 +3,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Checkbox from "./Checkbox";
 import Reset from "./Reset";
-
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators, State } from "../../store";
+import { bindActionCreators } from "redux";
+import { useState } from "react";
 function Search() {
+
+  const dispatch = useDispatch();
+  const { setCurrentPage, setResults, setLoading, setSearchText } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const filter = useSelector((state: State) => state.fetch);
+  const results = useSelector((state: State) => state.results);
+   const [searchWord, setSearchWord] =useState('');
+  function submitHandler(e:any){
+    e.preventDefault();
+    setSearchText(searchWord);
+    setCurrentPage(1);
+    setResults([]);
+    setLoading(true);
+  }
+  function searchWordHandler(e:any){
+    setSearchWord(e.target.value)
+  }
   return (
     <div className={"search"}>
       <form
         action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
+        onSubmit={submitHandler}
       >
         <div className={"searchElements"}>
-          <button className={"searchButton"}>
+          <button className={"searchButton"} onClick={submitHandler}>
             <FontAwesomeIcon icon={faSearch} className={"searchIcon"} />
           </button>
           <input
             className={"searchBar"}
             type="text"
             placeholder={"Søg Produkter"}
+            onChange={searchWordHandler}
           />
         </div>
 
