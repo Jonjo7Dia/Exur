4dd2435a2fb0d621e9dab835781716cde11af71b
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-13 21:37:13 -0300

    resetup of redux

--- src/components/rightPanel/Rightpanel.tsx
+++ src/components/rightPanel/Rightpanel.tsx
@@ -1,19 +1,17 @@
 import './RightPanel.css';
 import Intro from './Intro';
 import Filter from '../filter/Filter'
-import Pagination from '../pagination/Pagination';
 import Results from '../results/Results';
-import { useSelector } from 'react-redux';
-import {  FetchState } from "../../store/fetchReducer";
+
 
 function RightPanel(){
-  const searchItems:any = useSelector<FetchState>((state) => state);
+
 
     return <div className={'rightPanel'}>
         <Intro />
         <div className={'filterMobile'}> <Filter/></div>
         <Results />
-        {!searchItems.loading && <Pagination />}
+        {/* {!searchItems.loading && <Pagination />} */}
     </div>
 }
 
--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-
-import {fetchReducer} from './fetchReducer';
-
- const store = createStore(fetchReducer);
-
- export default store;
